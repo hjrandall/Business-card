947741262e401ab244cf96143613a774027c8628
haydenvasion <134477310+haydenvasion@users.noreply.github.com>
2023-06-13 20:31:58 -0700

    got a picture up and running

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
-  <v-btn>hi momssssss</v-btn>
+  <v-card id="card">
+    <img id="profile-pic" src="./assets/profilepic.jpg" alt="profile picture">
+  </v-card>
 </template>
 
 <style scoped>
+#card{
+  width: 600px;
+  margin-top: 20px;
+  height: 700px;
+}
+#profile-pic {
+  margin-top: 20px;
+  margin-left: 35%;
+  width: 150px;
+  height: 216px;
+  border-radius: 20px;
+}
 </style>
